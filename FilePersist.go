8576f93aa85b44043ee70fc914a28c92c1925cce
Alex Lopez <alex.lopezt@gmail.com>
2021-03-11 19:34:28 +0100

    Refactor

--- FilePersist.go
+++ FilePersist.go
@@ -3,34 +3,10 @@ package storage
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
 	"io"
 	"os"
 	"sync"
-	"time"
 )
-
-//ShortDate stores date in format yyyy-MM-dd
-type ShortDate time.Time
-
-func (Date ShortDate) MarshalJSON() ([]byte, error) {
-	t := time.Time(Date)
-	if y := t.Year(); y < 0 || y >= 10000 {
-		return nil, errors.New("Time.MarshalJSON: year outside of range [0,9999]")
-	}
-
-	return []byte(t.Format(`"02/01/2006"`)), nil
-}
-
-func (Date ShortDate) UnmarshalJSON(b []byte) error {
-	var s string
-	if err := json.Unmarshal(b, &s); err != nil {
-		return err
-	}
-	t, _ := time.Parse("2006-01-02", s)
-	Date = ShortDate(t)
-	return nil
-}
 
 var lock sync.Mutex
 var Marshal = func(v interface{}) (io.Reader, error) {
@@ -70,8 +46,4 @@ func LoadFromFile(path string, v interface{}) error {
 	}
 	defer f.Close()
 	return Unmarshal(f, v)
-}
-
-func Version() string {
-	return "0.0.1"
 }
